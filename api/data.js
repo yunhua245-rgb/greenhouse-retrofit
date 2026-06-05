30954294a1b4037c60e44162c5ad781da553d8bf
yunhua245-rgb <yunhua245@gmail.com>
2026-06-05 17:37:06 +0800

    refactor: 改读 projects/greenhouse/data.json

--- api/data.js
+++ api/data.js
@@ -2,7 +2,7 @@
 // Data persisted in GitHub repo file: data.json
 
 const GITHUB_REPO = 'yunhua245-rgb/greenhouse-retrofit';
-const GITHUB_FILE = 'data.json';
+const GITHUB_FILE = 'projects/greenhouse/data.json';
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 module.exports = async function handler(req, res) {
